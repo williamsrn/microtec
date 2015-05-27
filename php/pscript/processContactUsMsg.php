cc743c5e9eac50532722ce5c3bb99a45ab7787c6
Ralph <Ralph@i7-2600k.PK5001Z>
2015-05-27 10:31:45 -0500

    added mailer to send email about ContactUs inquiry

--- php/pscript/processContactUsMsg.php
+++ php/pscript/processContactUsMsg.php
@@ -43,13 +43,60 @@ if($rowCnt > 0){
 $result->close(); 
 //echo "userId: $userId";
 $sql = "INSERT INTO contact_us_msg (user_id, name_on_msg, subject_id, msg) VALUES ('$userId', '$name', '$subj', '$msg')"; 
-if($conn->query($sql) === true){    
-?>
-    <div>
-        <h4>Your message was successfully sent.</h4>
-        <p>Thank you for your inquiry.  We will respond shortly to the email you provided, <?php echo $email ?>.</p>
-    </div>
-<?php
+if($conn->query($sql) === true){
+    $mail = new PHPMailer;
+
+    //SMTP DEBUG 0,1,2,3,4
+    //$mail->SMTPDebug = 3;                                 // Enable verbose debug output
+    //$mail->Debugoutput = 'html'; 
+    
+    
+    $mail->isSMTP();                                        // Set mailer to use SMTP
+    $message = 'We have recieved an inquiry via ContactUs page.  Details to follow: <br><br>'
+            . 'Name:  ' . $name . '<br>'
+            . 'Email:  ' .$email . '<br>'
+            . 'Subject:  ' . $subj . '<br>'
+            . 'Message:  ' . $msg;
+    if($isLive){
+        /* PROD */
+        $mail->Host = 'a2ss15.a2hosting.com';                   // Specify main and backup SMTP servers                                    
+        $mail->Username = 'william5';                           // SMTP username
+        $mail->Password = 'host*!1';                            // SMTP password
+    }else{
+        /* LOCAL */
+        $mail->Host = 'smtp-mail.outlook.com';                  // Specify main and backup SMTP servers                                    
+        $mail->Username = 'microtecconsulting@outlook.com';     // SMTP username
+        $mail->Password = 'mtcrnw123';                          // SMTP password
+    }
+    
+    $mail->SMTPAuth = true;                                 // Enable SMTP authentication
+    $mail->SMTPSecure = 'tls';                              // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                      // TCP port to connect to
+    $mail->From = 'webmaster@microtecconsulting.com';
+    $mail->FromName = 'MicroTec ContactUs';
+    $mail->addAddress('microtecconsulting@outlook.com', 'ContactUs');                   // Add a recipient
+    $mail->addReplyTo('webmaster@microtecconsulting.com', 'Information');
+    $mail->isHTML(true);                                   // Set email format to HTML
+    $mail->Subject = 'Contact Us Message';
+    $mail->Body    = $message;                              //HTML Message  (if true)
+    $mail->AltBody = 'alt body message';
+    try {
+        $success = $mail->send();
+
+        if($success) {
+        ?>
+            <div>
+                <h4>Your message was successfully sent.</h4>
+                <p>Thank you for your inquiry.  We will respond shortly to the email you provided, <?php echo $email ?>.</p>
+            </div>
+        <?php
+        }else{
+            echo "Mailer Error: " . $mail->ErrorInfo;
+        }
+
+    } catch (Exception $ex) {    
+        echo $ex;      
+    }
 }else{
 ?>
     <div>
